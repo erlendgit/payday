15a61d8f9728f4b6c3ce39a004e1fffdb31c8633
Erlend ter Maat <etermaat@hfm.nl>
2016-10-16 20:40:49 +0200

    Refactor the run function - the content of the columns seemed more natural in the context of the csv file. The logic was abstracted enough by putting it in a calculator.

--- src/Processor.php
+++ src/Processor.php
@@ -1,37 +1,34 @@
 <?php
 
 namespace Payday;
+
 use Payday\Calculator;
 
 class Processor {
 
-  protected $filename;
-  protected $date;
+    protected $filename;
+    protected $date;
 
-  public function __construct($filename) {
-    $this->filename = $filename;
-    $this->date = strtotime("first day of january");
-  }
-
-  public function run() {
-    $fh = fopen($this->filename, 'w');
-    if ($fh) {
-      fputcsv($fh, ['Month', 'Payday', 'Bonusday']);
-      foreach (range(0, 11) as $month) {
-        fputcsv($fh, $this->processMonth($month));
-      }
-      fclose($fh);
+    public function __construct($filename) {
+        $this->filename = $filename;
+        $this->date = strtotime("first day of january");
     }
-  }
-  
-  protected function processMonth($no) {
-    $month = strtotime('+' . $no . ' months', $this->date);
-    $calculator = new Calculator($month);
-    return [
-      date('F', $month),
-      date('Y-m-d', $calculator->getPayDay()),
-      date('Y-m-d', $calculator->getBonusDay()),
-    ];
-  }
+
+    public function run() {
+        $fh = fopen($this->filename, 'w');
+        if ($fh) {
+            fputcsv($fh, ['Month', 'Payday', 'Bonusday']);
+            foreach (range(0, 11) as $offset) {
+                $month = strtotime('+' . $offset . ' months', $this->date);
+                $calculator = new Calculator($month);
+                fputcsv($fh, [
+                    date('F', $month),
+                    date('Y-m-d', $calculator->getPayDay()),
+                    date('Y-m-d', $calculator->getBonusDay()),
+                ]);
+            }
+            fclose($fh);
+        }
+    }
 
 }
